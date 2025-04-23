e2c13de0b4bdf4cdf94f5e9fd1fb41d1d255ff86
Daniel Grabowski <grabowski.daniel@gmail.com>
2025-04-23 17:00:37 +0200

    code addition

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -3,9 +3,21 @@ x = 120;
 y = 400;
 img;
 
+loadImage(path) {
+this.img = new Image();
+this.img.src = path;
+
+}
+
 moveRight(){
     console.log('Moving right');
     
 }
+
+moveLeft(){
+
+        
+}
+
 
 }
