c0c241fe666935e4c3a819ef2a3e7674e6f691d9
Daniel Grabowski <grabowski.daniel@gmail.com>
2025-04-25 17:00:31 +0200

    getting further with the el-pollo-loco project

--- js/game.js
+++ js/game.js
@@ -1,9 +1,10 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();	
 
 function init(){
 canvas=document.getElementById('canvas');
-world = new World(canvas);
+world = new World(canvas, keyboard);
 
 
 
@@ -11,3 +12,49 @@ console.log('My Character is: ', world['character']);
 
 
 }
+
+window.addEventListener('keydown', (e) => {
+    if(e.keyCode == 39) {
+        keyboard.RIGHT = true;
+        world.character.moveRight();
+    } else if(e.keyCode == 37) {
+        keyboard.LEFT = true;
+        world.character.moveLeft();
+    } else if(e.keyCode == 38) {
+        keyboard.UP = true;
+        world.character.jump();
+    } else if(e.keyCode == 40) {
+        keyboard.DOWN = true;
+    } else if(e.keyCode == 32) {
+        keyboard.SPACE = true;
+    }
+    
+    console.log(keyboard);
+
+   console.log(e);
+    
+
+});
+
+window.addEventListener('keyup', (e) => {
+    if(e.keyCode == 39) {
+        keyboard.RIGHT = false;
+        world.character.moveRight();
+    } else if(e.keyCode == 37) {
+        keyboard.LEFT = false;
+        world.character.moveLeft();
+    } else if(e.keyCode == 38) {
+        keyboard.UP = false;
+        world.character.jump();
+    } else if(e.keyCode == 40) {
+        keyboard.DOWN = false;
+    } else if(e.keyCode == 32) {
+        keyboard.SPACE = false;
+    }
+    
+    console.log(keyboard);
+
+   console.log(e);
+    
+
+});
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,10 +4,26 @@ y = 280;
 img;
 height = 150;
 width = 100;
+speed = 0.15;
+
+imageCache = {};
+currentImage = 0;
 
 loadImage(path) {
 this.img = new Image();
 this.img.src = path;
+
+}
+
+loadImages(arr) {
+
+arr.forEach((path) => {
+
+let img = new Image();
+img.src = path;
+this.imageCache[path] = img;
+
+});
 
 }
 
@@ -17,8 +33,9 @@ moveRight(){
 }
 
 moveLeft(){
-
-        
+    setInterval(() => {
+        this.x -= this.speed;
+    }, 1000/60)
 }
 
 
