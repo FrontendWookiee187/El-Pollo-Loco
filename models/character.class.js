5317f5b0b5841024c22cf75e91c3dbbf2ece3444
Daniel Grabowski <grabowski.daniel@gmail.com>
2025-04-28 16:59:11 +0200

    edited pathes for imgs and files

--- models/character.class.js
+++ models/character.class.js
@@ -2,17 +2,29 @@ class Character extends MovableObject {
 
     height = 300;
     width = 150;
-    y = 150;
+    y = 10;
     speed = 10;
 
     IMAGES_WALKING =[
 
-        '../img/2_character_pepe/2_walk/W-21.png',
-        '../img/2_character_pepe/2_walk/W-22.png',
-        '../img/2_character_pepe/2_walk/W-23.png',
-        '../img/2_character_pepe/2_walk/W-24.png',
-        '../img/2_character_pepe/2_walk/W-25.png',
-        '../img/2_character_pepe/2_walk/W-26.png',            
+        './img/2_character_pepe/2_walk/W-21.png',
+        './img/2_character_pepe/2_walk/W-22.png',
+        './img/2_character_pepe/2_walk/W-23.png',
+        './img/2_character_pepe/2_walk/W-24.png',
+        './img/2_character_pepe/2_walk/W-25.png',
+        './img/2_character_pepe/2_walk/W-26.png',            
+    ];
+
+    IMAGES_JUMPING =[
+        './img/2_character_pepe/3_jump/J-31.png',
+        './img/2_character_pepe/3_jump/J-32.png',
+        './img/2_character_pepe/3_jump/J-33.png',
+        './img/2_character_pepe/3_jump/J-34.png',
+        './img/2_character_pepe/3_jump/J-35.png',
+        './img/2_character_pepe/3_jump/J-36.png',
+        './img/2_character_pepe/3_jump/J-37.png',
+        './img/2_character_pepe/3_jump/J-38.png',
+        './img/2_character_pepe/3_jump/J-39.png',
     ]
 
     world;
@@ -20,9 +32,10 @@ class Character extends MovableObject {
     
 
     constructor() {
-        super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
-     this.loadImages(this.IMAGES_WALKING);
-
+        super().loadImage('./img/2_character_pepe/2_walk/W-21.png');
+        this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
+        this.applayGravity();
         this.animate();
         
 
@@ -46,12 +59,17 @@ class Character extends MovableObject {
 
         setInterval(() => {
 
+            if(this.isAboveGround()){
+                this.playAnimation(this.IMAGES_JUMPING); // Play jumping animation
+            } else {
+
             if(this.world.keyboard.RIGHT || this.world.keyboard.LEFT){               
 
         //walk animation  
         this.playAnimation(this.IMAGES_WALKING);      
         
     }
+}
         },50);
     }
  
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -6,9 +6,25 @@ height = 150;
 width = 100;
 speed = 0.15;
 otherDirection = false;
+speedY = 0;
+acceleration = 2.5;
 
 imageCache = {};
 currentImage = 0;
+
+applayGravity() {
+
+setInterval(() => {
+    if(this.isAboveGround()){
+    this.y -= this.speedY;
+    this.speedY -= this.acceleration; // Gravity effect
+}}, 1000 / 25);
+    
+}
+
+isAboveGround() {
+    return this.y < 130
+}
 
 loadImage(path) {
 this.img = new Image();
