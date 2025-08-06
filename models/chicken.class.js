f2be3ed2670da22bec1f0da1c28cb6cbca67e74f
Daniel Grabowski <grabowski.daniel@gmail.com>
2025-08-06 17:14:46 +0200

    Add JSDoc comments to Chicken class
    Added detailed JSDoc annotations to the Chicken class in models/chicken.class.js, including descriptions for properties, methods, and constructor. This improves code readability and provides better documentation for future maintenance.

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,59 +1,115 @@
+/**
+ * @fileoverview Normal chicken enemy class for El Pollo Loco game.
+ * Represents standard-sized chicken enemies with walking animation and knockout behavior.
+ * @author Frontend Wookiee
+ * @version 1.0.0
+ */
+
+/**
+ * Chicken class representing normal-sized chicken enemies.
+ * Extends MovableObject to inherit movement and collision detection capabilities.
+ * Features walking animation, knockout states, and continuous leftward movement.
+ * 
+ * @class
+ * @extends MovableObject
+ */
 class Chicken extends MovableObject{
     
+    /**
+     * Width of the chicken in pixels.
+     * @type {number}
+     * @default 80
+     */
     width = 80;
+    
+    /**
+     * Height of the chicken in pixels.
+     * @type {number}
+     * @default 80
+     */
     height = 80;
+    
+    /**
+     * Y-coordinate position of the chicken.
+     * @type {number}
+     * @default 350
+     */
     y = 350;
-    isKO = false; // Zustand des Huhns (k.o. oder nicht)
+    
+    /**
+     * Knockout state flag indicating if the chicken is defeated.
+     * @type {boolean}
+     * @default false
+     */
+    isKO = false;
 
+    /**
+     * Array of image paths for chicken walking animation.
+     * @type {string[]}
+     * @constant
+     */
     IMAGES_WALKING = [
         './img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         './img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
         './img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',                
     ]
 
-    IMAGES_WALKING_SMALL = [
-        './img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
-        './img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
-        './img/3_enemies_chicken/chicken_small/1_walk/3_w.png',
-]; 
+    /**
+     * Array of image paths for knockout/dead animation.
+     * @type {string[]}
+     * @constant
+     */
+    IMAGES_KO =[
+        './img/3_enemies_chicken/chicken_normal/2_dead/dead.png'
+    ];
 
-IMAGES_KO =[
-    './img/3_enemies_chicken/chicken_normal/2_dead/dead.png'
-];
-
+    /**
+     * Creates a new Chicken instance.
+     * Initializes the chicken with random position and speed, loads animations,
+     * sets up collision offsets, and starts the animation loop.
+     * 
+     * @constructor
+     */
     constructor() {
         super().loadImage('./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
         this.loadImages(this.IMAGES_KO);
         
-        this.x = 450 + Math.random() * 800; // Vergrößerter Bereich für mehr Abstand (von 500 auf 800)
+        this.x = 450 + Math.random() * 800;
         this.speed = 0.15 + Math.random() * 0.5;
 
-        // Offsets für präzise Kollisionserkennung
         this.offset = {
-            top: 5,    // Abstand von oben
-            bottom: 5, // Abstand von unten
-            left: 0,   // Minimaler Offset für direkten Kontakt
-            right: 0   // Minimaler Offset für direkten Kontakt
+            top: 5,
+            bottom: 5,
+            left: 0,
+            right: 0
         };
 
         this.animate();
     }
 
+    /**
+     * Handles animation and movement behavior for the chicken.
+     * Sets up two intervals: one for movement logic and one for animation playback.
+     * When alive, moves left continuously. When knocked out, falls down and plays death animation.
+     * 
+     * @method
+     * @returns {void}
+     */
     animate() {
         setInterval(() => {
             if (!this.isKO) {
-                this.moveLeft(); // Bewegt das Huhn kontinuierlich nach links
+                this.moveLeft();
             } else {
-                this.y += 5; // Lässt das Huhn nach unten fallen, wenn es k.o. ist
+                this.y += 5;
             }
         }, 1000 / 60);
 
         setInterval(() => {
             if (this.isKO) {
-                this.playAnimation(this.IMAGES_KO); // Spielt die K.O.-Animation ab
+                this.playAnimation(this.IMAGES_KO);
             } else {
-                this.playAnimation(this.IMAGES_WALKING); // Spielt die Laufanimation ab
+                this.playAnimation(this.IMAGES_WALKING);
             }
         }, 200);
     }
